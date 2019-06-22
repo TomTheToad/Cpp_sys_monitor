f61e5c1c184aa8d3f2611bf2020a37c9fdbe1ded
TomTheToad <tomthetoad@protonmail.com>
2019-06-22 17:51:46 +0000

    Restored Util.h to default by removing return type from getStream func. This isn't necesis being taken as an argument. Added next function getCpuPercent to ProcessParser.h. Will need to refactor

--- ProcessParser.h
+++ ProcessParser.h
@@ -54,10 +54,11 @@ string ProcessParser::getVmSize(string pid) {
   string name = "VmData";
   ifstream stream;
 
+  // Define file path
   string path = (Path::basePath() + pid + Path::statusPath());
 
   // Open stream
-  ifstream stream = Util::getStream(path, stream);
+  Util::getStream(path, stream);
 
   while (std::getline(stream, line)) {
     // Search ea line
@@ -75,28 +76,60 @@ string ProcessParser::getVmSize(string pid) {
   return to_string(result);
 }
 
+string ProcessParser::getCpuPercent(string pid) {
+
+  // Fields
+  string line;
+  string value;
+  float result;
+  ifstream stream;
+
+  // Define file path
+  string path = (Path::basePath() + pid + Path::statPath());
+
+  // Open stream
+  // TODO: util takes a stream but returns a stream?
+  Util::getStream(path, stream);
+  getline(stream, line);
+  string str = line;
+  istringstream buf(str);
+  istream_iterator<string> beg(buf), end;
+  vector<string> values(beg, end);
+
+  // extract stats
+  float utime = stof(ProcessParser::getProcUpTime(pid));
+  float stime = stof(values[14]);
+  float cutime = stof(values[15]);
+  float cstime = stof(values[16]);
+  float starttime = stof(values[21]);
+  // TODO: getSysUpTime() to be defined
+  float uptime = ProcessParser::getSysUpTime();
+  float freq = sysconf(_SC_CLK_TCK);
+  float total_time = utime + stime + cutime + cstime;
+  float seconds = uptime - (starttime / freq);
+  result = 100.0 * ((total_time / freq) / seconds);
+
+  // return
+  return to_string(result);
+}
+
 /*
-string ProcessParser::getVmsSize(string pid)
+string ProcessParser::getCpuPercent(string pid)
 {
     string line;
-    //Declaring search attribute for file
-    string name = "VmData";
     string value;
     float result;
-    // Opening stream for specific file
-    ifstream stream = Util::getStream((Path::basePath() + pid +
-Path::statusPath())); while(std::getline(stream, line)){
-        // Searching line by line
-        if (line.compare(0, name.size(),name) == 0) {
-            // slicing string line on ws for values using sstream
-            istringstream buf(line);
-            istream_iterator<string> beg(buf), end;
-            vector<string> values(beg, end);
-            //conversion kB -> GB
-            result = (stof(values[1])/float(1024*1024));
-            break;
-        }
-    }
-    return to_string(result);
+    ifstream stream = Util::getStream((Path::basePath()+ pid + "/" +
+Path::statPath())); getline(stream, line); string str = line; istringstream
+buf(str); istream_iterator<string> beg(buf), end; vector<string> values(beg,
+end); // done!
+    // acquiring relevant times for calculation of active occupation of CPU for
+selected process float utime = stof(ProcessParser::getProcUpTime(pid)); float
+stime = stof(values[14]); float cutime = stof(values[15]); float cstime =
+stof(values[16]); float starttime = stof(values[21]); float uptime =
+ProcessParser::getSysUpTime(); float freq = sysconf(_SC_CLK_TCK); float
+total_time = utime + stime + cutime + cstime; float seconds = uptime -
+(starttime/freq); result = 100.0*((total_time/freq)/seconds); return
+to_string(result);
 }
  */
--- util.h
+++ util.h
@@ -8,7 +8,7 @@ class Util {
 public:
   static std::string convertToTime(long int input_seconds);
   static std::string getProgressBar(std::string percent);
-  static std::ifstream getStream(std::string path, std::ifstream &stream);
+  static void getStream(std::string path, std::ifstream &stream);
 };
 
 std::string Util::convertToTime(long int input_seconds) {
@@ -47,7 +47,7 @@ std::string Util::getProgressBar(std::string percent) {
 }
 
 // wrapper for creating streams
-std::ifstream Util::getStream(std::string path, std::ifstream &stream) {
+void Util::getStream(std::string path, std::ifstream &stream) {
   stream.open(path, std::ifstream::in);
   if (!stream && !stream.is_open()) {
     stream.close();
