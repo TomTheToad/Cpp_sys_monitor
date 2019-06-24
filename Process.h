9ece177153dbc8276732d1ac9144db592688d873
TomTheToad <tomthetoad@protonmail.com>
2019-06-24 00:03:52 +0000

    Finished stubs for Process >>> constructor and return string

--- Process.h
+++ Process.h
@@ -7,44 +7,42 @@ It contains relevant attributes as shown below
 */
 class Process {
 private:
-    string pid;
-    string user;
-    string cmd;
-    string cpu;
-    string mem;
-    string upTime;
+  string pid;
+  string user;
+  string cmd;
+  string cpu;
+  string mem;
+  string upTime;
 
 public:
-    Process(string pid){
-        this->pid = pid;
-        this->user = ProcessParser::getProcUser(pid);
-        //TODOs:
-        //complete for mem
-        //complete for cmd
-        //complete for upTime
-        //complete for cpu
-    }
-    void setPid(int pid);
-    string getPid()const;
-    string getUser()const;
-    string getCmd()const;
-    int getCpu()const;
-    int getMem()const;
-    string getUpTime()const;
-    string getProcess();
-};
-void Process::setPid(int pid){
+  Process(string pid) {
     this->pid = pid;
-}
-string Process::getPid()const {
-    return this->pid;
-}
-string Process::getProcess(){
-    if(!ProcessParser::isPidExisting(this->pid))
-        return "";
-    this->mem = ProcessParser::getVmSize(this->pid);
-    this->upTime = ProcessParser::getProcUpTime(this->pid);
-    this->cpu = ProcessParser::getCpuPercent(this->pid);
+    this->user = ProcessParser::getProcUser(pid);
+    this->mem = ProcessParser::getVmSize(pid);
+    this->cmd = ProcessParser::getCmd(pid);
+    this->upTime = ProcessParser::getProcUpTime(pid);
+    this->cpu = ProcessParser::getCpuPercent(pid);
+  }
+  void setPid(int pid);
+  string getPid() const;
+  string getUser() const;
+  string getCmd() const;
+  int getCpu() const;
+  int getMem() const;
+  string getUpTime() const;
+  string getProcess();
+};
+void Process::setPid(int pid) { this->pid = pid; }
+string Process::getPid() const { return this->pid; }
+string Process::getProcess() {
+  if (!ProcessParser::isPidExisting(this->pid))
+    return "";
+  this->mem = ProcessParser::getVmSize(this->pid);
+  this->upTime = ProcessParser::getProcUpTime(this->pid);
+  this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   " + //TODO: finish the string! this->user + "   "+ mem...cpu...upTime...;
+  // return (this->pid + "   " + //TODO: finish the string! this->user + "   "+
+  // mem...cpu...upTime...);
+  return (this->pid + "   " + this->user + "   " + this->mem + "   " +
+          this->cmd + "   " + this->upTime + "   " + this->cpu);
 }
